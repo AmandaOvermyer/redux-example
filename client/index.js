749558b7c6efaa2412db2aacab5015da1282f6fb
santios <santios@gmail.com>
2017-01-27 16:10:38 +0000

    Little react starter

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Main = () => {
+    return (<div>Hello from react</div>);
+};
+
+ReactDom.render(<Main />, document.getElementById('app'));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
+};
